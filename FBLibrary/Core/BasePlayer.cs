9965a442ac6f484c086f670a9daa72ade35b55c1
Noxalus <yapogproject@gmail.com>
2013-09-21 04:43:53 +0200

    Added BasePlayer class in the common library project.

---
+++ FBLibrary/Core/BasePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace FBLibrary.Core
+{
+    public abstract class BasePlayer
+    {
+        protected int Id;
+        protected bool IsAlive;
+        protected LookDirection CurrentDirection;
+        protected TimeSpan InvincibleTime;
+        protected int CurrentBombAmount;
+        protected int TotalBombAmount;
+        protected int BombPower;
+
+        // TODO ?
+        protected Vector2 Position;
+        protected float Speed;
+
+        protected BasePlayer()
+        {
+            Speed = GameConfiguration.PlayerBaseSpeed;
+            // etc...
+        }
+    }
+}
---
+++ FBLibrary/Core/GameConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FBLibrary.Core
+{
+    public static class GameConfiguration
+    {
+        public const int PlayerBaseSpeed = 42;
+    }
+}
